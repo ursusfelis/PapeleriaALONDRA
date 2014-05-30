887f95b7be39e24e0d746a754d427f2ee01788a5
ursusfelis <joseantonio.contreras.gonzalez@gmail.com>
2014-05-30 14:54:33 -0500

    Revert "Revert "Readme""
    This reverts commit b5db41d2ea20cb9ad9442c6be23840416592ceda.

---
+++ PapeleriaALONDRA/MainAPP.Designer.cs
@@ -0,0 +1,47 @@
+﻿namespace PapeleriaALONDRA
+{
+    partial class MainApp
+    {
+        /// <summary>
+        /// Variable del diseñador requerida.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Limpiar los recursos que se estén utilizando.
+        /// </summary>
+        /// <param name="disposing">true si los recursos administrados se deben desechar; false en caso contrario.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Código generado por el Diseñador de Windows Forms
+
+        /// <summary>
+        /// Método necesario para admitir el Diseñador. No se puede modificar
+        /// el contenido del método con el editor de código.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.SuspendLayout();
+            // 
+            // MainApp
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(602, 273);
+            this.Name = "MainApp";
+            this.Text = "Papelería ALONDRA";
+            this.ResumeLayout(false);
+
+        }
+
+        #endregion
+    }
+}
+
